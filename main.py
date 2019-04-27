216f0fab06f371f85ac58e4c5c8ee232005e486c
ChrominskiMateusz <36238442+ChrominskiMateusz@users.noreply.github.com>
2019-04-27 22:16:55 +0200

    projectDone
    final commit

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Program to copy files with given extensions
+from given directory and all of its subdirectories
+to a given destination
+"""
+from os import walk
+from shutil import copy
+
+
+def getExt(str):
+    return str.split('.')[-1]
+
+
+def runMain(src, dst, ext):
+    for path, dirs, files in walk(src):
+        for file in files:
+            if getExt(file) in ext:
+                copy(path + '\\' + file, dst)
+
+
+if __name__ == '__main__':
+    runMain(input("Type in src\n"), input("Type in dst\n"), input("Type in exts\n"))
